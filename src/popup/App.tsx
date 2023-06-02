e251374ca1ebf8acf5f50ae2a5855634d439d7cb
cwjjjjj <93369602+cwjjjjj@users.noreply.github.com>
2023-06-02 20:24:51 +0800

    feat: 🎸 clean code

--- src/popup/App.tsx
+++ src/popup/App.tsx
@@ -1,55 +1,27 @@
-import {
-  BrowserRouter,
-  Route,
-  Routes,
-  MemoryRouter,
-  createMemoryRouter,
-  RouterProvider,
-} from "react-router-dom";
+import { createMemoryRouter, RouterProvider } from "react-router-dom";
 import Layout from "./components/Layout";
-import Mine from "./pages/Mine";
-import Explore from "./pages/Explore";
-import Follow from "./pages/Follow";
-import Login from "./pages/Login";
-import User from "./pages/User";
-import ProfileSetting from "./pages/ProfileSetting";
+import Home from "./page/Home";
 
 const routes = [
   {
     path: "/",
     element: <Layout />,
     children: [
-      { path: "login", element: <Login /> },
-      { path: "explore", element: <Explore /> },
-      { path: "mine", element: <Mine /> },
-      { path: "follow", element: <Follow /> },
-      { path: "user/:id/:name", element: <User /> },
-      { path: "profile-setting", element: <ProfileSetting /> },
+      {
+        path: "home",
+        element: <Home />,
+      },
     ],
   },
 ];
 
 export const router = createMemoryRouter(routes, {
-  initialEntries: ["/", "/explore"],
+  initialEntries: ["/", "/home"],
   initialIndex: 1,
 });
 
 function App() {
-  return (
-    <RouterProvider router={router} />
-    // <MemoryRouter basename="/" initialEntries={["/explore"]}>
-    //   <Routes>
-    //     <Route path="/" element={<Layout />}>
-    //       <Route path="login" element={<Login />} />
-    //       <Route path="explore" element={<Explore />} />
-    //       <Route path="mine" element={<Mine />} />
-    //       <Route path="follow" element={<Follow />} />
-    //       <Route path="user/:id/:name" element={<User />} />
-    //       <Route path="profile-setting" element={<ProfileSetting />} />
-    //     </Route>
-    //   </Routes>
-    // </MemoryRouter>
-  );
+  return <RouterProvider router={router} />;
 }
 
 export default App;
--- src/popup/components/Layout.tsx
+++ src/popup/components/Layout.tsx
@@ -1,48 +1,13 @@
 import { css } from "@emotion/react";
 import { Outlet, useLocation, useNavigate } from "react-router-dom";
-import Meun from "./Meun";
 import { useQuery } from "@tanstack/react-query";
-import { USER_PROFILE } from "../constants/api";
-import { get } from "../utils";
-import { User } from "../types";
 import { useRecoilState } from "recoil";
-import { userState } from "../store";
 import { useEffect } from "react";
-import { refreshTokenRequest } from "../utils/interceptor";
-import Header from "./Header";
 
 export default function Layout() {
   const location = useLocation();
   const navigator = useNavigate();
-  const [user, setUser] = useRecoilState(userState);
   console.log("location", location.pathname);
-
-  const { data: profile } = useQuery({
-    queryKey: [USER_PROFILE],
-    queryFn: async () =>
-      get<{ success: boolean; data: User }>(USER_PROFILE).then(
-        (res) => res.data.data
-      ),
-    onSuccess: (data) => {
-      setUser(data);
-      if (!data?.nickname || !data?.avatar) {
-        navigator("./profile-setting");
-      }
-    },
-    onError: (err) => {
-      console.log("err", err);
-    },
-  });
-
-  useEffect(() => {
-    refreshTokenRequest()
-      .then((res) => {
-        if ((res as any)?.response.status === 401) {
-          navigator("../login");
-        }
-      })
-      .catch((err) => {});
-  }, []);
 
   return (
     <div
@@ -55,11 +20,8 @@ export default function Layout() {
           height: calc(100% - 50px);
         `}
       >
-        <Header />
         <Outlet />
       </main>
-      {location.pathname !== "/profile-setting" &&
-        location.pathname !== "/login" && <Meun />}
     </div>
   );
 }
